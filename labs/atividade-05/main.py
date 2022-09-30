58baa013a91eae1667cb964bc00194e6a6e41064
João Pedro Brustolin Szlachta <110430872+JoaoBSzlachta@users.noreply.github.com>
2022-09-29 23:40:57 -0300

    Update main.py
    Versão concluida

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,7 +1,32 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+  aux = 0
+  qtd = 0
+
+  if(len(figurinhas_da_maria) < len(figurinhas_do_joao)):
+    for i in range(len(figurinhas_da_maria)):
+      for j in range(len(figurinhas_do_joao)):
+        if(figurinhas_da_maria[i] == figurinhas_do_joao[j]):
+          aux = aux + 1
+
+    qtd = len(figurinhas_da_maria) - aux
+
+  elif(len(figurinhas_da_maria) > len(figurinhas_do_joao)):
+    for i in range(len(figurinhas_do_joao)):
+      for j in range(len(figurinhas_da_maria)):
+        if(figurinhas_do_joao[i] == figurinhas_da_maria[j]):
+          aux = aux + 1
+          
+    qtd = len(figurinhas_do_joao) - aux
+
+  else:
+    for i in range(len(figurinhas_da_maria)):
+      for j in range(len(figurinhas_do_joao)):
+        if(figurinhas_da_maria[i] == figurinhas_do_joao[j]):
+          aux = aux + 1
+          
+    qtd = len(figurinhas_da_maria) - aux
+    
+    return qtd
 
 
 
